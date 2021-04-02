6e22a3cfdeaf1d2c64fe932bcebee8430833ad0b
Candice Scott-Mason <cscottmason23@gmail.com>
2021-04-01 20:04:15 -0400

    Completed Assignment

--- index.js
+++ index.js
@@ -1,1 +1,50 @@
 // Code your solution in this file!
+
+
+function distanceFromHqInBlocks(pickupLocation){
+    let result;
+    result = pickupLocation - 42;
+        return Math.abs(result);
+    
+}
+
+function distanceFromHqInFeet(pickuplocation){
+    let distance;
+    distance = distanceFromHqInBlocks(pickuplocation);
+    let result2 = distance * 264;
+        return result2;
+}
+
+function distanceTravelledInFeet(a,b){
+    let travelInFeet = Math.abs(a-b)*264;
+    return travelInFeet;
+}
+
+function calculatesFarePrice(a,b){
+    let destination = Math.abs(a-b)*264;
+    //let travelInFeet = Math.abs(a-b)*264;
+    //let fare;
+
+    if (destination < 400){
+        return 0
+    }
+    
+    else if( destination > 400 && destination < 2000){
+        return (destination - 400) * Number.parseFloat('0.02');
+    }
+
+    else if ( destination > 2000 && destination < 2500){
+        return 25;
+    }
+
+    
+    
+    else if ( destination > 2500){
+        return "cannot travel that far";
+    }
+
+
+    
+}
+    
+    
